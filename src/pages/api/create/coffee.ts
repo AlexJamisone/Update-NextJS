1da58f74e39c61d015ca8df51db6ca0380f48842
AlexJamisone <business.homeit@gmail.com>
2023-02-05 16:16:41 +0300

    debug coffee Data update

--- src/pages/api/create/coffee.ts
+++ src/pages/api/create/coffee.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prismadb'
 import { getTasty } from '../../../lib/wrapApi'
 import Cors from 'cors'
+import { WrapApiCoffeeData } from '../../../lib/types'
 
 const cors = Cors({
 	methods: ['GET'],
@@ -37,35 +38,21 @@ export default async function handler(
 			({ img: item1 }) =>
 				!dbCoffee.some(({ img: item2 }: Coffee) => item2 === item1)
 		)
-		const filterDataByDbPrice = dbCoffee.filter(
-			({ price: item1 }) =>
-				!apiCoffee.some(({ price: item2 }) => item2 === item1)
-		)
-		const filterDataByApiPrice = apiCoffee.filter(
-			(coffee1) =>
-				!dbCoffee.some(
-					(coffee2) =>
-						coffee2.price === coffee1.price &&
-						coffee2.name === coffee1.name
-				)
-		)
-		//Problem with updateMany by id
-		// const arrayCoffeePrice = filterDataByApiPrice.map(coffee => coffee.price)
-		// const arrayCoffeeDbId = filterDataByDbPrice.map(coffee => coffee.id)
-
 		await prisma.coffee.createMany({
 			data: filterDataByApi,
 		})
-		if ((filterDataByApiPrice || filterDataByDbPrice) !== undefined) {
-			await prisma.coffee.updateMany({
+		const promises = apiCoffee.map(async ({ img, price }) => {
+			const records = await prisma.coffee.update({
 				where: {
-					id: filterDataByDbPrice[0]?.id,
+					img: img,
 				},
 				data: {
-					price: filterDataByApiPrice[0]?.price,
+					price: price,
 				},
 			})
-		}
+			return records
+		})
+		await Promise.all(promises)
 		res.status(200).json({ message: 'create succses!', dbCoffee })
 	} catch (error: any) {
 		console.log(error)
