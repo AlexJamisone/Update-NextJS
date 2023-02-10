e4e7f9233b2fcb3b39079b46e2aecc12bf47295c
AlexJamisone <business.homeit@gmail.com>
2023-02-10 10:29:14 +0300

    Add cron lib qstash

--- src/pages/api/update-task.ts
+++ src/pages/api/update-task.ts
@@ -1,11 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { updatePrice } from "../../lib/updatePrice"
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const response = await updatePrice()
-        res.status(200).json({message: 'price is update ✔', response})
-    } catch (error) {
-        console.log(error)
-        res.json({message: "task faild"})
+import { NextApiRequest, NextApiResponse } from 'next'
+import { updatePrice } from '../../lib/updatePrice'
+import { verifySignature } from '@upstash/qstash/nextjs'
+
+export const config = {
+    api: {
+        bodyParser: false
     }
 }
+
+async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	try {
+		const response = await updatePrice()
+		res.status(200).json({ message: 'price is update ✔', response })
+	} catch (error) {
+		console.log(error)
+		res.json({ message: 'task faild' })
+	}
+}
+
+export default verifySignature(handler)
